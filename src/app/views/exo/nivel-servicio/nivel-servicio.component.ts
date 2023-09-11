84f6a91e6aa01566184073e69fc1d54b2fec69df
Sepa123 <sebastian.retamal.contreras@gmail.com>
2023-09-11 14:56:35 -0300

    update

--- src/app/views/exo/nivel-servicio/nivel-servicio.component.ts
+++ src/app/views/exo/nivel-servicio/nivel-servicio.component.ts
@@ -3,6 +3,9 @@ import { FormControl, FormGroup, FormBuilder, Validators, AbstractControl, Valid
 import { PortalTransyanezService } from "src/app/service/portal-transyanez.service";
 import { TIService } from 'src/app/service/ti.service';
 import { NSBeetrackRango } from 'src/app/models/nsBeetrackRango.interface';
+import Swal from 'sweetalert2';
+
+
 
 @Component({
   selector: 'app-nivel-servicio',
@@ -17,12 +20,91 @@ export class NivelServicioComponent {
 
   isDescargar : boolean = false
 
+
+  //DATOS PARA EL ID Y ASIGNAR VALOR
+  selectedRouteId: number | null = null; 
+  Valor_ruta : number | null = null;
+  selectedRouteValue: number | null = null;
+  routeValue: number | null = null;
+  valoresActualizados : any [] = []
+  tablaDesplegada: boolean = false;
+  server : string = "";
+  id_usuario : number  | null = null;
+  idMs : String = "" ;
+
+
   constructor(
-    private service: PortalTransyanezService, private TIservice: TIService,
+    private service: PortalTransyanezService, private TIservice: TIService
   ) {
 
    }
 
+  // Método para capturar el ID de la ruta al hacer clic en un elemento de la lista
+  captureRouteId(routeId: number, routeValue: number|null)  {
+  
+    if(routeId!= null){
+      //se selecciona el ID de la ruta
+      this.selectedRouteId = routeId;
+      //se asigna el valor de la ruta a traves de del input
+      this.selectedRouteValue = routeValue;
+      //se obtiene de la sesion del usuario los datos del id 
+      this.id_usuario = parseInt(sessionStorage['id'])
+      this.server = sessionStorage['server']
+      this.idMs= this.server+ "-"+this.id_usuario
+      //se crea el objeto con los datos
+      const rutaValores = {id_ruta : this.selectedRouteId, valor_ruta : this.selectedRouteValue, id_user : this.id_usuario, ids_user: this.idMs}
+      if(this.selectedRouteValue != null && this.selectedRouteId != null){
+        // if( this.valoresActualizados.includes((valor : any)=> valor.id_ruta === this.selectedRouteId )){
+        // }
+
+        const objetoAActualizar = this.valoresActualizados.find(objeto => objeto.id_ruta === routeId);
+
+        // Verifica si se encontró el objeto
+        if (objetoAActualizar) {
+          // Actualiza la propiedad en el objeto
+          objetoAActualizar.valor_ruta = this.selectedRouteValue;
+        } else {
+          console.log('No se encontró el objeto con el ID especificado.');
+          this.valoresActualizados.push(rutaValores)
+        }
+        console.log( this.valoresActualizados, "objeto")
+      }
+    }   
+  }
+  guardarValores() {
+    //al guardar los datos se almacena solo los values del objeto
+    const valores = this.valoresActualizados.map(valoresArray => ({
+      id_ruta: valoresArray.id_ruta,
+      valor_ruta: valoresArray.valor_ruta,
+      id_user: valoresArray.id_user,
+      ids_user: valoresArray.ids_user
+      
+    }));
+    // se cuentan cada uno de los regristrados guardados en el array
+    let registrosRealizados = 0
+    valores.forEach(()=>{
+      registrosRealizados ++;
+    })
+    let conteo = `${registrosRealizados}`
+    this.TIservice.update_valor_ruta(valores).subscribe(
+      (response) => {
+        console.log(valores);
+        Swal.fire(
+          'Ingreso Realizado Correctamente. Registros insertados :' + conteo,
+          )
+        this.valoresActualizados = []
+        console.log('Valores de rutas actualizados correctamente', response, valores);
+      },
+      (error) => {
+
+        Swal.fire(
+          'Carga Finalizada con errores. Se registran : 0 cambios, favor validar.!',
+          )
+        console.error('Error al actualizar los valores de rutas', error);
+        
+      }
+    ); 
+  }
    formatearFecha(fecha : string){
     return fecha.replaceAll("-", "")
    }
@@ -82,6 +164,7 @@ export class NivelServicioComponent {
    }
    ngOnInit(){
     // this.ns_beetrack_rango_fecha('20230801','20230802') 
+  
    }
    
   downloadVehiculos(){
@@ -95,8 +178,9 @@ export class NivelServicioComponent {
   ns_beetrack_rango_fecha(fecha_inicio : string, fecha_termino : string){
     
     this.TIservice.get_ns_beetrack_por_rango_fecha(fecha_inicio,fecha_termino).subscribe(data => {
+      this.tablaDesplegada = true;
       this.nsBeetrack = data
     })
   }
   
-}
+  }
