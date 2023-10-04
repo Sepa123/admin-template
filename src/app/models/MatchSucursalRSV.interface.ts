dfc48629ad428c667f0dfd98286392cad7491b39
Sepa123 <sebastian.retamal.contreras@gmail.com>
2023-10-04 16:37:32 -0300

    Delete MatchSucursalRSV.interface.ts

--- src/app/models/MatchSucursalRSV.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface MatchSucursalRSV {
-    Id:               number;
-    bar_code:         string;
-    id_sucursal:      number;
-    sucursal:         string;
-    stock:            boolean;
-
-}
