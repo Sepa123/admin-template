3217d3c7958f865bcc56d04c8932013070d9a933
Nobm1 <74340698+Nobm1@users.noreply.github.com>
2025-02-19 13:36:26 -0300

    UPdate correcion llamada telefonica
    Se agregaba +56 adicional sin necesidad.

--- src/app/views/mercadolibre/citaciones/citaciones.component.ts
+++ src/app/views/mercadolibre/citaciones/citaciones.component.ts
@@ -980,7 +980,7 @@ mostrarAlerta(mensaje: string, tipo: 'success' | 'error' | 'warning'): void {
   }
 
 makePhoneCall(phoneNumber: string): void {
-  const fullPhoneNumber = `+56${phoneNumber}`;
+  const fullPhoneNumber = `${phoneNumber}`;
   console.log('Intentando llamar al número:', fullPhoneNumber); // Registro de depuración
   const isWindows = navigator.platform.indexOf('Win') > -1;
   if (isWindows) {
