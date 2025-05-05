1fc035d1009dac43a35f9ab65735f21fbd62abea
Nobm1 <74340698+Nobm1@users.noreply.github.com>
2025-05-05 16:25:18 -0400

    Update Finanzas module
    Se ajusta buscadores en vez de select en el modada de agregar.

--- src/app/views/finanzas/test/test.component.ts
+++ src/app/views/finanzas/test/test.component.ts
@@ -342,6 +342,7 @@ export class TestComponent {
 
 
 
+
     if (this.formDescuentos.valid){
 
 
@@ -366,7 +367,67 @@ export class TestComponent {
     }
   }
 
+  filtroPatente: string = '';
+  patentesFiltradas: any[] = [...this.seleccionPantente];
+  patenteSeleccionada: string = ''; // Nombre de la patente seleccionada
+  idPatenteSeleccionada: number | null = null;
 
+  seleccionarPatente(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    const patenteSeleccionada = inputElement.value;
+
+    // Buscar el ID correspondiente a la patente seleccionada  Pantente Razon_social
+
+    const patente = this.seleccionPantente.find(
+      (razon) => razon.Patente === patenteSeleccionada
+    );
+
+    if (patente) {
+      this.idPatenteSeleccionada = patente.Id;
+
+      // Actualizar el valor del control en el formulario
+    this.formDescuentos.patchValue({
+      Pantente: this.idPatenteSeleccionada,
+    });
+      console.log('ID de la patente seleccionada:', this.idPatenteSeleccionada);
+    } else {
+      this.idPatenteSeleccionada = null; // Si no coincide, reinicia el ID
+      console.log('Patente no encontrada');
+      // Reiniciar el valor del control en el formulario
+    this.formDescuentos.patchValue({
+      Pantente: null,
+    });
+    }
+  }
+
+  filtroRazonSocial: string = ''; // 
+  razonesFiltradas: any[] = [...this.seleccionRazonSocial]; // Lista filtrada
+  razonSocialSeleccionada: string = ''; // Nombre seleccionado en el input
+  idRazonSocialSeleccionada: number | null = null; 
+  seleccionarRazonSocial(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    const nombreSeleccionado = inputElement.value;
+
+    // Buscar el ID correspondiente al nombre seleccionado
+    const razon = this.seleccionRazonSocial.find(
+      (razon) => razon.Nombre_razon === nombreSeleccionado
+    );
+
+    if (razon) {
+      this.idRazonSocialSeleccionada = razon.Id;
+      this.formDescuentos.patchValue({
+        Razon_social: this.idRazonSocialSeleccionada,
+      });
+      console.log('ID seleccionado:', this.idRazonSocialSeleccionada);
+    } else {
+      this.idRazonSocialSeleccionada = null; // Si no coincide, reinicia el ID
+      console.log('Razón social no encontrada');
+
+      this.formDescuentos.patchValue({
+        Razon_social: null,
+      });
+    }
+  }
 
   DescargarNS() {
     const workbook = new ExcelJS.Workbook();
@@ -374,7 +435,7 @@ export class TestComponent {
   
     // Encabezados de la tabla
     worksheet.columns = [
-      { header: 'Fecha Cobro', key: 'fechaCobro', width: 15 },
+      { header: 'Periodo (mes)', key: 'fechaCobro', width: 15 },
       { header: 'Ingresado Por', key: 'ingresadoPor', width: 20 },
       { header: 'Operación', key: 'operacion', width: 15 },
       { header: 'Centro Operación', key: 'centroOperacion', width: 20 },
